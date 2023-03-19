9dd707fee41984f0d71c98d2bb60def1c4959819
Adam Due Hansen <adamduehan@gmail.com>
2023-03-19 12:05:24 +0100

    Adds game engine.

--- src/lib/game.test.ts
+++ src/lib/game.test.ts
@@ -1,6 +1,125 @@
-import { describe, test, expect, vi } from 'vitest';
-import { createGame } from './game';
+import { describe, test, expect } from 'vitest';
+import { createGame, GameState, SelectResult } from './game';
 
 describe('game', () => {
-  describe('select', () => {});
+  describe('createGame', () => {
+    test('should create card pairs', () => {
+      // Arrange
+      const expectedMatches = [
+        {
+          id: 1,
+          matched: false,
+        },
+        {
+          id: 2,
+          matched: false,
+        },
+      ];
+      const game = createGame({
+        pokemonIds: [1, 2],
+      });
+
+      // Act
+      const matches = game.getPairs();
+
+      // Assert
+      expect(matches).toEqual(expectedMatches);
+    });
+
+    test('should set game state to idle', () => {
+      // Arrange
+      const expectedState: GameState = 'idle';
+
+      // Act
+      const game = createGame({
+        pokemonIds: [1, 2],
+      });
+
+      // Assert
+      expect(game.getState()).toEqual(expectedState);
+    });
+  });
+
+  describe('select', () => {
+    test('should return awaiting result on first select', () => {
+      // Arrange
+      const expectedState: SelectResult = 'await';
+      const game = createGame({
+        pokemonIds: [1, 2],
+      });
+
+      // Act
+      const result = game.select(1);
+
+      // Assert
+      expect(result).toEqual(expectedState);
+    });
+
+    test('should return match result on second select', () => {
+      // Arrange
+      const expectedState: SelectResult = 'match';
+      const game = createGame({
+        pokemonIds: [1, 2],
+      });
+
+      // Act
+      game.select(1);
+      const result = game.select(1);
+
+      // Assert
+      expect(result).toEqual(expectedState);
+    });
+
+    test('should return invalid result on second select', () => {
+      // Arrange
+      const expectedState: SelectResult = 'invalid';
+      const game = createGame({
+        pokemonIds: [1, 2],
+      });
+
+      // Act
+      game.select(1);
+      const result = game.select(2);
+
+      // Assert
+      expect(result).toEqual(expectedState);
+    });
+  });
+
+  describe('gameState', () => {
+    test('should await after first select', () => {
+      // Arrange
+      const expectedState: GameState = 'await';
+      const game = createGame({
+        pokemonIds: [1, 2],
+      });
+
+      // Act
+      game.select(1);
+
+      // Assert
+      expect(game.getState()).toEqual(expectedState);
+    });
+
+    test('should be idle after second select', () => {
+      // Arrange
+      const expectedState: GameState = 'idle';
+      const gameWithMatch = createGame({
+        pokemonIds: [1, 2],
+      });
+      const gameWithInvalid = createGame({
+        pokemonIds: [1, 2],
+      });
+
+      // Act
+      gameWithMatch.select(1);
+      gameWithMatch.select(1);
+
+      gameWithInvalid.select(1);
+      gameWithInvalid.select(2);
+
+      // Assert
+      expect(gameWithMatch.getState()).toEqual(expectedState);
+    });
+  });
 });
--- src/lib/game.ts
+++ src/lib/game.ts
@@ -1,10 +1,42 @@
-interface Game {
-  readonly firstSelectedId?: number;
-  select: (id: number) => void;
+export interface Pair {
+  id: number;
+  matched: boolean;
 }
 
-export function createGame(): Game {
+export type SelectResult = 'await' | 'match' | 'invalid';
+export type GameState = 'idle' | 'await';
+
+export interface Game {
+  getPairs: () => Pair[];
+  getState: () => GameState;
+  select: (id: number) => SelectResult;
+}
+
+export function createGame(options: { pokemonIds: number[] }): Game {
+  const { pokemonIds } = options;
+  let selectedId: number;
+  let pairs = pokemonIds.map((pokemonId): Pair => {
+    return {
+      id: pokemonId,
+      matched: false,
+    };
+  });
+  let gameState: GameState = 'idle';
   return {
-    select: function () {},
+    getState: () => gameState,
+    getPairs: () => pairs,
+    select: function (id: number) {
+      if (selectedId === undefined) {
+        selectedId = id;
+        gameState = 'await';
+        return 'await';
+      } else if (selectedId === id) {
+        gameState = 'idle';
+        return 'match';
+      }
+
+      gameState = 'idle';
+      return 'invalid';
+    },
   };
 }
