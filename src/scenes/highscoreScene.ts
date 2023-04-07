766065d5d6697dbe083fbf181765ec8bde2b4494
Adam Due Hansen <adamduehan@gmail.com>
2023-04-07 08:00:59 +0200

    Moves flashing to component file.

--- src/scenes/highscoreScene.ts
+++ src/scenes/highscoreScene.ts
@@ -1,39 +1,6 @@
+import flashing, { FlashingComp } from '@/components/flashing';
 import kctx from '@/lib/kctx';
-import { Comp, GameObj, TextComp } from 'kaboom';
-
-interface FlashingComp extends Comp {
-  disableFlashing: boolean;
-}
-
-function flashing(
-  interval: number,
-  options?: {
-    disable: boolean;
-  }
-): FlashingComp {
-  let pauseFlashing = options?.disable || false;
-  let animationTimer = 0;
-  return {
-    disableFlashing: pauseFlashing,
-    update: function (this: GameObj<FlashingComp>) {
-      if (this.disableFlashing) {
-        this.hidden = false;
-        animationTimer = 0;
-        return;
-      }
-
-      animationTimer += kctx.dt();
-
-      if (animationTimer <= interval) {
-        this.hidden = true;
-      } else if (animationTimer > interval && animationTimer <= interval * 2) {
-        this.hidden = false;
-      } else {
-        animationTimer = 0;
-      }
-    },
-  };
-}
+import { GameObj, TextComp } from 'kaboom';
 
 function AddScoreInputObject(score: number): GameObj {
   const scoreName: string[] = Array(3).fill('_');
