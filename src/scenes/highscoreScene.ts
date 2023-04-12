6d7fadf3f2ceb9e99245d1e9451887c96bb12d48
Adam Due Hansen <adamduehan@gmail.com>
2023-04-12 07:28:19 +0200

    Styles the score box.

--- src/scenes/highscoreScene.ts
+++ src/scenes/highscoreScene.ts
@@ -9,27 +9,31 @@ function AddScoreInputObject(
 ): GameObj {
   let currentScoreNameLetterIndex = 0;
 
-  const container = kctx.add([]);
+  const container = kctx.add([
+    kctx.rect(400, 200, {
+      radius: 10,
+    }),
+    kctx.color(kctx.BLACK),
+    kctx.pos(kctx.width() / 2, 28),
+    kctx.outline(5, kctx.YELLOW),
+    kctx.anchor('top'),
+  ]);
 
   container.add([
     kctx.text(`Your score ${score}`),
-    kctx.pos(kctx.width() / 2, 100),
-    kctx.anchor('center'),
+    kctx.anchor('top'),
+    kctx.pos(0, 20),
   ]);
 
-  container.add([
-    kctx.text('Enter initials'),
-    kctx.pos(kctx.width() / 2, 200),
-    kctx.anchor('center'),
-  ]);
+  container.add([kctx.text('Enter name'), kctx.pos(0, 80), kctx.anchor('top')]);
 
   const scoreNameLetterObjects = Array(3)
     .fill('_')
     .map((scoreNameLetter, index): GameObj<TextComp | FlashingComp> => {
       return container.add([
         kctx.text(scoreNameLetter),
-        kctx.pos(kctx.width() / 2 - 100 + index * 100, 300),
-        kctx.anchor('center'),
+        kctx.pos(-100 + index * 100, 130),
+        kctx.anchor('top'),
         flashing(0.5, {
           disable: index > 0,
         }),
