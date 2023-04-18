564b1c4077c9331cb381c567be95047fa1f09d2e
Adam Due Hansen <adamduehan@gmail.com>
2023-04-18 23:32:24 +0200

    Removes client folder.

--- client/src/lib/kctx.ts
+++
@@ -1,7 +0,0 @@
-import kaboom from 'kaboom';
-
-const kctx = kaboom({
-  global: false,
-});
-
-export default kctx;
--- client/src/lib/scenes.ts
+++
@@ -1,7 +0,0 @@
-const scenes = {
-  game: 'game_scene',
-  preload: 'preload_scene',
-  highscore: 'highscore_scene',
-};
-
-export default scenes;
