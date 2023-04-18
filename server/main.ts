59a132abc42ca69e3eeee65e935ac061c53830f2
Adam Due Hansen <adamduehan@gmail.com>
2023-04-18 22:45:27 +0200

    Port is loaded from environment variable.

--- server/main.ts
+++ server/main.ts
@@ -4,6 +4,7 @@ import {
   Router,
 } from 'https://deno.land/x/oak@v12.1.0/mod.ts';
 import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
+import { load } from 'https://deno.land/std@0.184.0/dotenv/mod.ts';
 
 interface Score {
   id: string;
@@ -41,6 +42,14 @@ const app = new Application();
 app.use(oakCors());
 app.use(router.routes());
 
+const configData = await load();
+const port = Number(configData['PORT']);
+
+if (isNaN(port)) {
+  throw new Error('Port environment variable is not a number!');
+}
+
+console.log(`Running server on port ${port}...`);
 await app.listen({
-  port: 8000,
+  port: Number(port),
 });
