0bd603a0592158e8f532ee84247bfa7f42fc9169
Adam Due Hansen <adamduehan@gmail.com>
2023-05-09 23:10:49 +0200

    Updates vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const environmentVariables = loadEnv(mode, process.cwd());
+
+  return {
+    base: environmentVariables.VITE_BASE,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
+  };
 });
