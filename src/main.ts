18fc5c3ef8323ee92c95651655c54b1093fc85f4
Adam Due Hansen <adamduehan@gmail.com>
2023-05-10 07:05:52 +0200

    Sets load URL for card.

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import preloadScene from './scenes/preloadScene';
 
 const assetRoot = import.meta.env.VITE_ASSET_URL;
 
-kctx.loadSprite('card', '/images/card.png', {
+kctx.loadSprite('card', `${import.meta.env.VITE_BASE}images/card.png`, {
   sliceX: 4,
   sliceY: 1,
 });
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_ASSET_URL: string;
   readonly VITE_MAX_ID: string;
   readonly VITE_WS_URL: string;
+  readonly VITE_BASE: string;
 }
 
 interface ImportMeta {
