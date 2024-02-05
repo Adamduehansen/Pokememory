43800b97d25b0aa3452d5753085f25e5f9b601c6
Adam Due Hansen <adamduehan@gmail.com>
2024-02-05 07:21:06 +0100

    Adds css flip animation.

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,5 @@
 import { JSX } from "preact/jsx-runtime";
+import classnames from "classnames";
 
 type Props = JSX.HTMLAttributes<HTMLButtonElement> & {
   flipped: boolean;
@@ -8,18 +9,24 @@ type Props = JSX.HTMLAttributes<HTMLButtonElement> & {
 export function Card(
   { flipped, pokemonImageUrl, ...rest }: Props,
 ): JSX.Element {
-  const imageComponent = flipped === true
-    ? <img src={pokemonImageUrl} alt="Pokemon" />
-    : (
-      <img
-        src="/pokemon_card_back.gif"
-        alt="Backside of a Pokemon Card, pixelated"
-      />
-    );
-
   return (
-    <button {...rest}>
-      {imageComponent}
-    </button>
+    <div class="flip-container">
+      <button
+        class={classnames("flip-button", {
+          "flipped": flipped,
+        })}
+        {...rest}
+      >
+        <div class="front">
+          <img
+            src="/pokemon_card_back.gif"
+            alt="Backside of a Pokemon Card, pixelated"
+          />
+        </div>
+        <div class="back">
+          <img src={pokemonImageUrl} alt="Pokemon" />
+        </div>
+      </button>
+    </div>
   );
 }
